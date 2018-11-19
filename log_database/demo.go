15a1e8ca87b2dde9164d1d211d5b7275779a83bf
scottnuma <scott.numamoto@gmail.com>
2018-11-18 20:32:01 -0800

    Update demo

--- log_database/demo.go
+++ log_database/demo.go
@@ -1,57 +1,23 @@
 package log_database
 
 import (
-	"crypto/rand"
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
 )
 
 // Demonstrate the ability to create, write to and read from a database.
 func Demo() {
-	os.Remove("./foo.db")
 
-	db, err := sql.Open("sqlite3", "./foo.db")
+	db, err := sql.Open("sqlite3", "./sample.glog")
 	checkError(err)
 	defer db.Close()
 
-	checkError(CreateTable(db))
+	logEntries, err := GetAllLogs(db)
+	hash := logEntries[0].Hash
 
-	// create an example log entry
-	storedHash := make([]byte, 4)
-	rand.Read(storedHash)
-	storedPrevHash := make([]byte, 4)
-	rand.Read(storedPrevHash)
-
-	storedLogEntry := LogEntry{
-		Hash:      storedHash,
-		RecNo:     1,
-		Timestamp: 2,
-		Accuracy:  3.0,
-		PrevHash:  storedPrevHash,
-		Value:     []byte("some value"),
-		Sig:       []byte("some signature"),
-	}
-	logEntries := []LogEntry{storedLogEntry}
-
-	checkError(AppendLogEntry(db, logEntries))
-
-	rows, err := db.Query("select hash, recno from log_entry")
-	checkError(err)
-	defer rows.Close()
-
-	for rows.Next() {
-		var recNo int
-		var hash []byte
-
-		err = rows.Scan(&hash, &recNo)
-		checkError(err)
-
-		fmt.Printf("hash: %x\n", hash)
-		fmt.Printf("recno: %d\n", recNo)
-	}
-	checkError(rows.Err())
+	present, err := HashPresent(db, hash)
+	fmt.Printf("hash present: %t\n", present)
 }
 
 func checkError(err error) {
--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/tonyyanga/gdp-replicate/gdplogd"
-    "github.com/tonyyanga/gdp-replicate/policy"
+	"github.com/tonyyanga/gdp-replicate/gdplogd"
+	"github.com/tonyyanga/gdp-replicate/log_database"
+	"github.com/tonyyanga/gdp-replicate/policy"
 )
 
 func main() {
-    // Just to test import
-    var msgType policy.MessageType
-    msgType = 1
+	// Just to test import
+	var msgType policy.MessageType
+	msgType = 1
 
-    var addr gdplogd.HashAddr
+	var addr gdplogd.HashAddr
 
-    fmt.Println(msgType)
-    fmt.Println(addr)
+	fmt.Println(msgType)
+	fmt.Println(addr)
+	log_database.Demo()
 }
-
