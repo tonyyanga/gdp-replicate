711663fa7890650d69454becf1d22a60ad3b536b
Steven Wu <stevenwuyinze@berkeley.edu>
2018-12-11 08:22:20 +0000

    5,0.1,2,naive

--- benchmark/topology.py
+++ benchmark/topology.py
@@ -18,11 +18,14 @@ class GDPSimulationTopo(Topo):
 
 # topos = {'simple': (lambda: GDPSimulationTopo(3, None)), 'lossy': (lambda: GDPSimulationTopo(3, 0.01))}
 
-if len(sys.argv) != 2:
-    print ('NUM_LOG_SERVER')
+if len(sys.argv) != 5:
+    print ('NUM_LOG_SERVER, WRITE_ITERVAL, FANOUT, ALGO')
     sys.exit(2)
 
 NUM_LOG_SERVER = int(sys.argv[1])
+WRITE_ITERVAL = float(sys.argv[2])
+FANOUT = int(sys.argv[3])
+ALGO = sys.argv[4]
 PORT = 10262
 WRITE_INTERVAL = 0.2
 
@@ -32,25 +35,33 @@ if __name__ == '__main__':
     net = Mininet(topo=topo)
     net.start()
     # net.pingAll()
-    os.system('rm -f *.db')
+    path = ','.join(sys.argv[1:])
+    os.system("mkdir " + path)
+    os.system('rm -f {0}/*.db'.format(path))
+    os.system('rm -f {0}/*.log'.format(path))
     for i in range(NUM_LOG_SERVER):
-        create_fresh_logdb(str(i) + ".db")
+        create_fresh_logdb("{0}/{1}.db".format(path, i))
     log_servers = net.hosts[:NUM_LOG_SERVER]
     writer = net.hosts[-1]
     for i, server in enumerate(log_servers):
-        peers_addr = ['{0}:{1}'.format(h.IP(), PORT) for h in log_servers if h != server]
+        # server.cmdPrint('tcpdump port {0} -i h{1}-eth0 -w {2}/{3}.pcap &'.format(PORT, i + 1, path, i))
+        peers_addr = ['{0}:{1}'.format(h.IP(), PORT) for h in log_servers
+                if h != server]
         peers_addr_str = ",".join(peers_addr)
         db_file = str(i) + ".db"
-        server.cmdPrint('../gdp-replicate', 
-                        str(i) + '.db',
+        server_cmd = ['sudo ../gdp-replicate',
+                        "{0}/{1}.db".format(path, i),
                         '{0}:{1}'.format(server.IP(), PORT),
                         ",".join(peers_addr),
-                         1,
-                        '2>', str(i) + '.log',
-                        '&')
-    writer.cmdPrint('python3 writer.py',
-                     NUM_LOG_SERVER, WRITE_INTERVAL,
-                     #'> writer.log',
+                         FANOUT,
+                        'naive',
+                        '2>', "{0}/{1}.log".format(path, i),
+                        '&']
+        if ALGO != 'naive':
+            server_cmd.remove('naive')
+        server.cmdPrint(server_cmd) 
+    writer.cmdPrint('sudo python3 writer.py',
+                     NUM_LOG_SERVER, WRITE_INTERVAL, path,
                     '&')
     time.sleep(1000)
     net.stop()
--- benchmark/writer.py
+++ benchmark/writer.py
@@ -4,13 +4,20 @@ import datetime
 from gdb_log_utils import *
 import sys
 
-if len(sys.argv) != 3:
-    print("NUMBER_LOG_SERVER, WRITE_INTERVAL")
-    sys.exit(2)
+if len(sys.argv) != 4:
+    print("NUMBER_LOG_SERVER, WRITE_INTERVAL, PATH")
+    sys.exit(3)
 
+
+PATH = sys.argv[3]
 LOGDB_NUM = int(sys.argv[1])
-GLOG_DB = [str(i) + '.db' for i in range(LOGDB_NUM)]
+GLOG_DB = [PATH + "/%s.db" % i for i in range(LOGDB_NUM)]
 WRITE_INTERVAL = float(sys.argv[2])
+MIN_DATA_SIZE = 1024
+MAX_DATA_SIZE = 1025
+CHURN_TIME = [200, 400]
+END_TIME = 600
+
 
 print("WRITER BEGINS")
 
@@ -19,9 +26,20 @@ if __name__ == '__main__':
     connections = [create_connection(file) for file in GLOG_DB]
     servers = list(range(LOGDB_NUM))
     cnt = 0
-    log_file = open("writer.log", "w")
-    while True:
+    log_file = open(PATH + "/writer.log", "w")
+    churn_file = open(PATH + "/churn.log", "w")
+    while cnt <= END_TIME:
+        if cnt in CHURN_TIME:
+            # randint is inclusive on both side
+            server_to_die = 0 #random.randint(0, LOGDB_NUM - 1)
+            wipe_all_records(PATH + "/%s.db" % server_to_die)
+            log = dict(timestamp=str(datetime.datetime.now()),
+                       written_cnt=cnt,
+                       server_id=server_to_die)
+            churn_file.write(str(log) + "\n")
+            churn_file.flush()
         curr_hash = get_hash(str(random.getrandbits(256)))
+        data_size = random.randint(MIN_DATA_SIZE, MAX_DATA_SIZE)
         curr_data = get_hash(str(random.getrandbits(1024)))
         curr_sig = get_hash(str(random.getrandbits(100)))
         prev_hash = written_hash[-1]
@@ -36,10 +54,10 @@ if __name__ == '__main__':
             conn.commit()
             log = dict(timestamp=str(datetime.datetime.now()),
                        write_cnt=cnt,
-                       server_chosen=chosen,
+                       server_id=i,
                        record_hash=curr_hash.hex().upper())
             log_file.write(str(log) + "\n")
-            log_file.flush()
+        log_file.flush()
         cnt += 1
         time.sleep(WRITE_INTERVAL)
-
+    print("writer finished.")
