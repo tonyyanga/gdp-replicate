19f65904e681c608114c310892362f7944627cc0
Tony Yang <tonyyanga@gmail.com>
2019-04-22 16:09:10 -0700

    implement encoding / decoding of messages

--- utils.go
+++ utils.go
@@ -6,10 +6,11 @@ import "C"
 
 import (
     "unsafe"
-)
+    "bytes"
+    "encoding/gob"
 
-import (
 	"github.com/tonyyanga/gdp-replicate/gdp"
+	"github.com/tonyyanga/gdp-replicate/policy"
 )
 
 func peerAddrToHash(addr C.PeerAddr) gdp.Hash {
@@ -19,11 +20,49 @@ func peerAddrToHash(addr C.PeerAddr) gdp.Hash {
 }
 
 func toCMsg(msg interface{}) C.Msg {
-	// TODO
-	return C.Msg{}
+    dest := &bytes.Buffer{}
+
+    encoder := gob.NewEncoder(dest)
+    gob.Register(&policy.NaiveMsgContent{})
+    gob.Register(&policy.GraphMsgContent{})
+
+    err := encoder.Encode(msg)
+    if err != nil {
+        panic(err)
+    }
+
+    // convert dest to a c array
+    length := dest.Len()
+    destBytes := dest.Bytes()
+    destCArray := C.malloc(C.size_t(C.int(length)))
+    copy(C.GoBytes(unsafe.Pointer(destCArray), C.int(length)), destBytes[:])
+
+	return C.Msg{
+        length: C.uint(length),
+        data: destCArray,
+    }
 }
 
 func toGoMsg(msg C.Msg) interface{} {
-	// TODO
-	return nil
+    // convert a c array to a buffer
+    length := msg.length
+    srcCArray := msg.data
+
+    var srcBytes []byte
+    copy(srcBytes, C.GoBytes(unsafe.Pointer(srcCArray), C.int(length)))
+
+    src := bytes.NewBuffer(srcBytes)
+
+    decoder := gob.NewDecoder(src)
+    gob.Register(&policy.NaiveMsgContent{})
+    gob.Register(&policy.GraphMsgContent{})
+
+    var resp interface{}
+
+    err := decoder.Decode(resp)
+    if err != nil {
+        panic(err)
+    }
+
+    return resp
 }
